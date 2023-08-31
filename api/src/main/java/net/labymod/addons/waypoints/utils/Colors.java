e03c8e3f7ee05e15e433e1778c4ae35266507fe3
MalikShr <mail.skw@proton.me>
2023-08-31 11:46:21 +0200

    Added Colors

---
+++ api/src/main/java/net/labymod/addons/waypoints/utils/Colors.java
@@ -0,0 +1,9 @@
+package net.labymod.addons.waypoints.utils;
+
+public class Colors {
+  public static final int LABEL_COLOR = -22016;
+  public static final int BRACKET_COLOR = -5592406;
+  public static final int VALUE_COLOR = -1;
+
+
+}
