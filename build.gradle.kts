6e206934d689a439d0d62669dbc456b616a172d3
Christian <r0bby@gmx.net>
2025-01-30 14:12:47 +0100

    Update labygradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
-    id("java-library")
-    id("net.labymod.gradle")
-    id("net.labymod.gradle.addon")
+    id("net.labymod.labygradle")
+    id("net.labymod.labygradle.addon")
 }
 
-group = "net.labymod.addons.waypoints"
-version = "1.0.0"
+val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+group = "net.labymod.addons"
+version = providers.environmentVariable("VERSION").getOrElse("0.0.1")
 
 labyMod {
     defaultPackageName = "net.labymod.addons.waypoints"
+
+    minecraft {
+        registerVersion(versions.toTypedArray()) {
+
+        }
+    }
+
     addonInfo {
         namespace = "labyswaypoints"
         displayName = "Laby's Waypoints"
@@ -19,75 +25,14 @@ labyMod {
         minecraftVersion = "*"
         version = System.getenv().getOrDefault("VERSION", "0.0.1")
     }
-
-    minecraft {
-        registerVersions(
-                "1.8.9",
-                "1.12.2",
-                "1.16.5",
-                "1.17.1",
-                "1.18.2",
-                "1.19.2",
-                "1.19.3",
-                "1.19.4",
-                "1.20.1",
-                "1.20.2",
-                "1.20.4"
-        ) { version, provider ->
-            configureRun(provider, version)
-        }
-
-        subprojects.forEach {
-            if (it.name != "game-runner") {
-                filter(it.name)
-            }
-        }
-    }
-
-    addonDev {
-        productionRelease()
-    }
 }
 
 subprojects {
-    plugins.apply("java-library")
-    plugins.apply("maven-publish")
-    plugins.apply("net.labymod.gradle")
-    plugins.apply("net.labymod.gradle.addon")
+    plugins.apply("net.labymod.labygradle")
+    plugins.apply("net.labymod.labygradle.addon")
 
     repositories {
         maven("https://libraries.minecraft.net/")
         maven("https://repo.spongepowered.org/repository/maven-public/")
-        mavenLocal()
-    }
-}
-
-fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionProvider, gameVersion: String) {
-    provider.runConfiguration {
-        mainClass = "net.minecraft.launchwrapper.Launch"
-        jvmArgs("-Dnet.labymod.running-version=${gameVersion}")
-        jvmArgs("-Dmixin.debug=true")
-        jvmArgs("-Dnet.labymod.debugging.all=true")
-        jvmArgs("-Dmixin.env.disableRefMap=true")
-
-        args("--tweakClass", "net.labymod.core.loader.vanilla.launchwrapper.LabyModLaunchWrapperTweaker")
-        args("--labymod-dev-environment", "true")
-        args("--addon-dev-environment", "true")
-    }
-
-    provider.javaVersion = JavaVersion.VERSION_17
-
-    provider.mixin {
-        val mixinMinVersion = when (gameVersion) {
-            "1.8.9", "1.12.2", "1.16.5" -> {
-                "0.6.6"
-            }
-
-            else -> {
-                "0.8.2"
-            }
-        }
-
-        minVersion = mixinMinVersion
     }
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,18 +1,10 @@
-version = "0.1.0"
-
-plugins {
-    id("java-library")
-}
+import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtension.ReferenceType
 
 dependencies {
+    labyProcessor()
     api(project(":api"))
 }
 
-labyModProcessor {
-    referenceType = net.labymod.gradle.core.processor.ReferenceType.DEFAULT
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+labyModAnnotationProcessor {
+    referenceType = ReferenceType.DEFAULT
 }
